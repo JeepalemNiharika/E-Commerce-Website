f74573c9cdf225b0586ecf2f59f113fc5f4e037c
Niharika <162446000+JeepalemNiharika@users.noreply.github.com>
2024-03-06 17:19:35 +0530

    Update viewpage.js

--- viewpage.js
+++ viewpage.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded",function(){
          <p id="dec">Description:</p><p id="description">${data.description}</p>
          <a href="cartpage.html"><button id="btn1" onclick="send(${data.id})">Add to cart</button></a>
          <a href="Buypage.html"><button id="btn3" onclick="send(${data.id})">Buy</button></a>
-         <a href="Index.html"><button id="btn2">Back to Home page</button></a>`;
+         <a href="index.html"><button id="btn2">Back to Home page</button></a>`;
 
    }
 
